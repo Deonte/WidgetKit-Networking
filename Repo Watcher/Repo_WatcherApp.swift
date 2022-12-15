d8d5fd2f2b3b4e95923d052e9d73ac3bbcd32ea4
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2022-12-14 23:25:58 -0500

    Initial Commit

---
+++ Repo Watcher/Repo_WatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Repo_WatcherApp.swift
+//  Repo Watcher
+//
+//  Created by Deonte Kilgore on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Repo_WatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
